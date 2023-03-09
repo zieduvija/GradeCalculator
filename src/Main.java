11b7c50b0a3021aedc84eb6322d388305ef7c6a2
Cibuļskis <kaspars.cibulskis@gmail.com>
2023-03-09 13:36:53 +0200

    Kritēriju ieviešana un to svara ievadīšana

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner ievade = new Scanner(System.in);
+
+        System.out.println("Ievadi studentu skaitu: ");
+        int studentuSkaits = ievade.nextInt();
+
+        String[] kriteriji = {"Apmeklējums", "Mājasdarbi", "Kontroldarbi", "Eksāmens"};
+
+        int[] svari = new int[kriteriji.length];
+
+        for(int i = 0 ; i<kriteriji.length; i++)
+        {
+            int procenti = 0;
+            System.out.println("Ievadi cik % svarīgs ir kritērijs: "+kriteriji[i]);
+            do{
+                if(procenti > 100 || procenti < 0)
+                    System.out.println("Ievadītais procentu skaits ir nederīgs!");
+                procenti = ievade.nextInt();
+            }while(procenti > 100 || procenti < 0);
+            svari[i] = procenti;
+        }
+
+
     }
 }
